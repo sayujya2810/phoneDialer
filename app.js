9ab6e3801e283d2891c3665c1b91c91367096e1f
Sayujya Malkan <sayujyamalkan@gamil.com>
2022-01-13 18:35:00 +0530

    finished w/o animation

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const inputMain = document.querySelector("#mainInput")
+// const inputMain = document.querySelector("#mainInput")
+const para = document.querySelector("#para")
 
 const addInput = (val) => {
-    inputMain.value += val;
+    // inputMain.value += val;
+    para.textContent += val;
 }
 
 const deleteText = () => {
-    input = document.querySelector("#mainInput")
-    text = input.value;
-    // console.log(text)
-    len = text.length;
-    // console.log(len)
-    text = text.substring(0,text.length - 1);
-    // console.log(text)
-    inputMain.value = text;
+
+    const p = document.querySelector("#para")
+    t = p.textContent;
+    l = t.length;
+    t = t.substring(0,t.length - 1);
+
+    p.textContent = t;
 }
