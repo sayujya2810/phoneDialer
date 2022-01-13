e3ee23051fab8461b6152595fec3174ab487c9cb
Sayujya Malkan <sayujyamalkan@gamil.com>
2022-01-13 17:37:27 +0530

    delete Function Added

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const inputMain = document.querySelector("#mainInput")
 
-console.log(inputMain.value);
-
 const addInput = (val) => {
     inputMain.value += val;
 }
 
+const deleteText = () => {
+    input = document.querySelector("#mainInput")
+    text = input.value;
+    // console.log(text)
+    len = text.length;
+    // console.log(len)
+    text = text.substring(0,text.length - 1);
+    // console.log(text)
+    inputMain.value = text;
+}
